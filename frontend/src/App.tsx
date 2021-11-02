314559f6f985e1fb513f9e7e9a1ba08df07227c0
anderson oliveira <arodrigodeoliveira@gmail.com>
2021-11-02 15:58:37 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,35 @@
+import BarChart from "components/BarCharts/Index";
+import DataTable from "components/DataTable/Index";
+import DonatChart from "components/DonutChart/Index";
+import Footer from "components/Footer/Index";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <DonatChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as Vendas</h2>
+        </div>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
