eb8ffe98fa69133fd75e6608185bf69bf35e1514
anderson oliveira <arodrigodeoliveira@gmail.com>
2021-11-05 20:10:03 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/Index.tsx
@@ -0,0 +1,36 @@
+import BarChart from "components/BarCharts/Index";
+import DataTable from "components/DataTable/Index";
+import DonatChart from "components/DonutChart/Index";
+import Footer from "components/Footer/Index";
+import NavBar from "components/NavBar";
+
+const Dashboard = () => {
+    return (
+        <>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary">Dashboard de Vendas</h1>
+
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+                        <DonatChart />
+                    </div>
+                </div>
+
+                <div className="py-3">
+                    <h2 className="text-primary">Todas as Vendas</h2>
+                </div>
+
+                <DataTable />
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Dashboard;
