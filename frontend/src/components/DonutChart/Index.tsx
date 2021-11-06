727d298af68d3bbe7887e8ad9b1ca2511678529b
anderson oliveira <arodrigodeoliveira@gmail.com>
2021-11-05 21:10:09 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/Index.tsx
+++ frontend/src/components/DonutChart/Index.tsx
@@ -25,8 +25,6 @@ const DonatChart = () => {
             })
     }, [])
 
-
-
     //    const mockData = {
     //        series: [477138, 499928, 444867, 220426, 473088],
     //        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
